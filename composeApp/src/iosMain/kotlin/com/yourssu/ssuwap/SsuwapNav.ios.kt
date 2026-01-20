90be11de6fc2f5821ee72cbca7d0b27e701c71a4
jun011026 <jun011026@gmail.com>
2026-01-20 15:20:08 +0900

    Camera Impl Each Platform

--- composeApp/src/iosMain/kotlin/com/yourssu/ssuwap/SsuwapNav.ios.kt
+++ composeApp/src/iosMain/kotlin/com/yourssu/ssuwap/SsuwapNav.ios.kt
@@ -1,36 +1,36 @@
-package com.yourssu.ssuwap
+    package com.yourssu.ssuwap
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.yourssu.ssuwap.navigation.HomeRoute
-import com.yourssu.ssuwap.navigation.extension.*
+    import androidx.compose.runtime.Composable
+    import androidx.navigation.compose.NavHost
+    import androidx.navigation.compose.rememberNavController
+    import com.yourssu.ssuwap.navigation.HomeRoute
+    import com.yourssu.ssuwap.navigation.extension.*
 
-@Composable
-context(appGraph: AppGraph)
-actual fun SsuwapNav() {
-    val navController = rememberNavController()
+    @Composable
+    context(appGraph: AppGraph)
+    actual fun SsuwapNav() {
+        val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = HomeRoute,
-    ) {
-        homeScreen(
-            onNavigateToImageSelect = navController::navigateToImageSelect,
-        )
-        cameraScreen(
-            onPhotoCaptured = { uri ->
-                navController.previousBackStackEntry
-                    ?.savedStateHandle
-                    ?.set("camera_result_key", uri)
-                navController.popBackStack()
-            }
-        )
-        imageSelectScreen(
-            onNavigateToCamera = navController::navigateToCamera,
-            onNavigateToTransformResult = navController::navigateToTransformResult,
+        NavHost(
             navController = navController,
-        )
-        transformResultScreen()
+            startDestination = HomeRoute,
+        ) {
+            homeScreen(
+                onNavigateToImageSelect = navController::navigateToImageSelect,
+            )
+            cameraScreen(
+                onPhotoCaptured = { uri ->
+                    navController.previousBackStackEntry
+                        ?.savedStateHandle
+                        ?.set("camera_result_key", uri)
+                    navController.popBackStack()
+                }
+            )
+            imageSelectScreen(
+                onNavigateToCamera = navController::navigateToCamera,
+                onNavigateToTransformResult = navController::navigateToTransformResult,
+                navController = navController,
+            )
+            transformResultScreen()
+        }
     }
-}
--- feature/camera/build.gradle.kts
+++ feature/camera/build.gradle.kts
@@ -10,5 +10,13 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.androidx.runtime.retain)
         }
+
+        androidMain.dependencies {
+            implementation(libs.androidx.camera.core)
+            implementation(libs.androidx.camera.camera2)
+            implementation(libs.androidx.camera.lifecycle)
+            implementation(libs.androidx.camera.view)
+            implementation(libs.accompanist.permissions)
+        }
     }
 }
